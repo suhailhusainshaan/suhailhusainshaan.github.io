2cdc52117c77d3fa28d8ae45f852608687b200d7
Suhail Husain <69467628+suhailhusainshaan@users.noreply.github.com>
2025-04-16 19:55:49 +0530

    Updated new wallpaper

--- files/images.js
+++ files/images.js
@@ -1,11 +1,11 @@
 const images = [
       {
         "url": "https://images.unsplash.com/photo-1612544448445-b8232cff3b6c",
-        "description": "This is a description for the first slide."
+        "description": "Have a good day Sir!"
       },
       {
         "url": "https://images.unsplash.com/photo-1610809376778-928ee2c3a561",
-        "description": "This is a description for the second slide."
+        "description": "Have a good day Sir!"
       },
       {
         "url": "files/img/3.jpg",
@@ -13,6 +13,10 @@ const images = [
       },
       {
         "url": "https://images.unsplash.com/photo-1566008889966-2bc46924ab06",
+        "description": "Have a good day Sir!"
+      },
+      {
+        "url": "https://images.unsplash.com/photo-1605816988069-b11383b50717",
         "description": "Have a good day Sir!"
       }
     ];
