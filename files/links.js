db5c021c265ab06eed79c840ba833c9a5a3b2f61
Suhail Husain <69467628+suhailhusainshaan@users.noreply.github.com>
2025-10-13 13:46:18 +0530

    Update links.js

--- files/links.js
+++ files/links.js
@@ -15,6 +15,11 @@ const links = [
       "link": "https://curitics.monday.com/boards/7855829113/views/173794093?userId=73284224"
     },
     {
+      "name": "Plane",
+      "image": "files/img/icons/plane.png",
+      "link": "https://app.plane.so/curitics-health/projects/4882b325-285a-421d-8a35-55073497d412/issues"
+    },
+    {
       "name": "Github",
       "image": "files/img/icons/github.png",
       "link": "https://github.com/curiticshealth/Risk-Adjustment"
@@ -30,4 +35,5 @@ const links = [
       "link": "https://docs.google.com/spreadsheets/d/1X9tZ44KQn8avtISEEEYUsMpXpx2kczylIEe1eqEeaJw/edit?gid=0#gid=0"
     }
   ];
+
 
