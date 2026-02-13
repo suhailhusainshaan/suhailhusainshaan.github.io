2340ef02a122544346f78e2bf801719ced2a5a74
Suhail Husain <suhail.husain111@gmail.com>
2026-02-13 14:56:19 +0530

    Video added (#2)
    * Add glass effect styles to index.html
    
    Added custom styles for glass effect to enhance UI.
    
    * Added videos in my homepage
    
    ---------
    
    Co-authored-by: Suhail <suhail.h@curiticshealth.com>

--- files/images.js
+++ files/images.js
@@ -1,24 +1,23 @@
 const images = [
-      {
-        "url": "https://www.wsupercars.com/wallpapers-regular/Tuners/2025-Brabus-1000-001-1080.jpg",
+    {
+        "url": "files/img/8.mp4",
         "description": "Have a good day Sir!"
-      },
-      {
-        "url": "https://wallpapercat.com/w/middle-retina/e/7/b/1646060-3840x2160-desktop-4k-land-rover-defender-wallpaper.jpg",
+    },
+    {
+        "url": "files/img/9.mp4",
         "description": "Have a good day Sir!"
-      },
-      {
+    },
+    {
         "url": "https://images.pexels.com/photos/733745/pexels-photo-733745.jpeg",
         "description": "Have a good day Sir!"
-      },
-      {
+    },
+    {
         "url": "https://images.pexels.com/photos/248747/pexels-photo-248747.jpeg",
         "description": "Have a good day Sir!"
-      },
-      {
+    },
+    {
         "url": "https://images.pexels.com/photos/1131575/pexels-photo-1131575.jpeg",
         "description": "Have a good day Sir!"
-      }
-      
-    ];
+    }
 
+];
